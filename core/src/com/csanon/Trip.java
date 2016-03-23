d460b2bb32e37652ce7869047492e8f3f353511e
Sean Amos <sjamos@wpi.edu>
2016-03-23 13:44:44 -0400

    Added initial trip, updated airplane and flight.

---
+++ core/src/com/csanon/Trip.java
@@ -0,0 +1,5 @@
+package com.csanon;
+
+public class Trip {
+	//TODO: Create Trip
+}
---
+++ core/src/com/csanon/TripBuilder.java
@@ -0,0 +1,24 @@
+package com.csanon;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public class TripBuilder {
+	private static TripBuilder instance = null;
+	
+	private TripBuilder() {
+		
+	}
+	
+	public TripBuilder getInstance() {
+		if (instance == null) {
+			instance = new TripBuilder();
+		}
+		return instance;
+	}
+	
+	public List<Trip> getTrips(Airport aDeparture, Airport aDestination, OffsetDateTime aDepartTime) {
+		//TODO: make a list of valid trips from aDeparture airport to a aDestination airport at the given time
+		return null;
+	}
+}
