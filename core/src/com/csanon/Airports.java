0b09ed74918896f584fc4d3cb92c1db10dbec7b2
magwitch324 <magwitch324@gmail.com>
2016-03-23 14:04:04 -0400

    Working on static airports

---
+++ core/src/com/csanon/Airports.java
@@ -0,0 +1,33 @@
+package com.csanon;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.csanon.server.FlightServer;
+import com.csanon.server.ServerFactory;
+
+public class Airports {
+
+	private final static HashMap<String, Airport> airports = new HashMap<String, Airport>();
+	private final static FlightServer server = ServerFactory.getServer();
+
+	public static void initialize() {
+		List<Airport> airportList = server.getAirports();
+		Map<String, Airport> airportMap = airportList.stream()
+				.collect(Collectors.toMap(Airport::getCode, Function.identity()));
+		airports.putAll(airportMap);
+	}
+
+	public static List<Airport> getAirports() {
+		return new LinkedList<Airport>(airports.values());
+	}
+
+	public static Airport getAirport(String code) {
+		return airports.get(code);
+	}
+
+}
