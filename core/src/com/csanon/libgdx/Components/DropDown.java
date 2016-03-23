80753e3f9dab07e98fc0b4b7c121e950875ebbad
argallo <argallo@wpi.edu>
2016-03-23 14:48:06 -0400

    dropdown functionality

--- core/src/com/csanon/libgdx/Components/DropDown.java
+++ core/src/com/csanon/libgdx/Components/DropDown.java
@@ -1,8 +1,10 @@
 package com.csanon.libgdx.Components;
 
+import java.util.List;
+
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox.SelectBoxStyle;
@@ -20,18 +22,25 @@ public class DropDown extends Group{
 		selectBoxStyle = new SelectBoxStyle();
 		selectBoxStyle.fontColor = Tint.BACKGROUND_COLOR;
 		selectBoxStyle.background = Assets.getInstance().getDrawable(Pic.Pixel);
-		selectBoxStyle.font = Assets.getInstance().getMidFont();
-		//selectBoxStyle.scrollStyle = ScrollPaneStyle;
-		//selectBoxStyle.scrollStyle.background = Assets.getInstance().getDrawable(Pic.Pixel);
-		selectBoxStyle.listStyle = new List.ListStyle();
-		selectBoxStyle.listStyle.font = Assets.getInstance().getMidFont();
+		selectBoxStyle.font = Assets.getInstance().getSmallFont();
+		selectBoxStyle.scrollStyle = new ScrollPaneStyle();
+		selectBoxStyle.scrollStyle.background = Assets.getInstance().getDrawable(Pic.Pixel);
+		selectBoxStyle.listStyle = new ListStyle();
+		selectBoxStyle.listStyle.font = Assets.getInstance().getSmallFont();
 		selectBoxStyle.listStyle.fontColorSelected = Color.RED;
 		selectBoxStyle.listStyle.fontColorUnselected = Color.ORANGE;
 		selectBoxStyle.listStyle.selection = Assets.getInstance().getDrawable(Pic.Pixel);
 		selectBoxStyle.listStyle.background = Assets.getInstance().getDrawable(Pic.Pixel);
 		selectBox = new SelectBox<String>(selectBoxStyle);
-		selectBox.setSize(100, 50);
+		selectBox.setSize(150, 50);
+		selectBox.setPosition(300, 300);
 		addActor(selectBox);
+	}
+	
+	public void setItems(List<String> newItems){
+		String[] array = new String[newItems.size()];
+		array = newItems.toArray(array);
+		selectBox.setItems(array);
 	}
 	
 	
