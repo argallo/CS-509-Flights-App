b57640354c4dfe1ecb0fcf63fcd5ed79e964b79f
Sean Amos <sjamos@wpi.edu>
2016-03-23 14:47:52 -0400

    Started working on multiage trip

--- core/src/com/csanon/Trip.java
+++ core/src/com/csanon/Trip.java
@@ -4,23 +4,48 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Trip {
-	private final Flight flight;
+	private final List<Flight> legs = new LinkedList<Flight>();
 	
 	public Trip(Flight aFlight) {
-		flight = aFlight;
+		legs.add(aFlight);
+	}
+	
+	public Trip(Flight aFlight1, Flight aFlight2 ) {
+		legs.add(aFlight1);
+		legs.add(aFlight2);
+	}
+	
+	public Trip(Flight aFlight1, Flight aFlight2, Flight aFlight3) {
+		legs.add(aFlight1);
+		legs.add(aFlight2);
+		legs.add(aFlight3);
 	}
 	
 	public String getLayoverTime() {
-		return "00:00";
+		if (legs.size() == 1) {
+			return "00:00";
+		} else if (legs.size() == 2) {
+			long difference = legs.get(1).getDepartureTime().toEpochSecond() -
+								legs.get(0).getArrivalTime().toEpochSecond();
+			int hours
+			return 
+		} else {
+			
+		}
 	}
 	
 	public String getTotalTime() {
-		return flight.getDuration();
+		if (legs.size() == 1) {
+			return legs.get(0).getDuration();
+		} else if (legs.size() == 2) {
+			
+		} else {
+			
+		}
+		
 	}
 	
 	public List<Flight> getLegs() {
-		List<Flight> legs = new LinkedList<Flight>();
-		legs.add(flight);
 		return legs;
 	}
 }
