d3575715df4bf91084cc58b1c5d925c16a865fc4
magwitch324 <magwitch324@gmail.com>
2016-03-23 15:09:03 -0400

    Finished changing server to handle airports and offsetdatetimes instead of strings

--- core/test/com/csanon/server/WPIFlightServerTest.java
+++ core/test/com/csanon/server/WPIFlightServerTest.java
@@ -2,15 +2,32 @@ package com.csanon.server;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.csanon.Airplane;
+import com.csanon.Airplanes;
 import com.csanon.Airport;
+import com.csanon.Airports;
 import com.csanon.Flight;
 
 public class WPIFlightServerTest {
+	
+	Airport airport;
+	OffsetDateTime date;
+	
+	@Before
+	public void setUp() throws Exception {
+		ZoneOffset offset = ZoneOffset.of("-00:00");
+		date = OffsetDateTime.of(2016, 5, 10, 0, 0, 0, 0, offset);
+		Airports.initialize();
+		Airplanes.initialize();
+		airport = Airports.getAirport("BOS");
+	}
 
 	@Test
 	public void testGetAirports() {
@@ -23,14 +40,14 @@ public class WPIFlightServerTest {
 	@Test
 	public void testGetDepartingFlights() {
 		FlightServer server = ServerFactory.getServer();
-		List<Flight> result = server.getFlightsDeparting("BOS", "2016_05_10");
+		List<Flight> result = server.getFlightsDeparting(airport, date);
 		assertTrue(result.size() > 0);
 	}
 
 	@Test
 	public void testGetArrivingFlights() {
 		FlightServer server = ServerFactory.getServer();
-		List<Flight> result = server.getFlightsArrivingAt("BOS", "2016_05_10");
+		List<Flight> result = server.getFlightsArrivingAt(airport, date);
 		assertTrue(result.size() > 0);
 	}
 
