4bcbed57bd3d59dbcdc96c03e54c44f6368d85a9
argallo <argallo@wpi.edu>
2016-03-23 13:50:04 -0400

    UI 3

---
+++ core/src/com/csanon/libgdx/Components/DropDown.java
@@ -0,0 +1,10 @@
+package com.csanon.libgdx.Components;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+
+public class DropDown extends Group{
+
+	
+	
+}
--- core/src/com/csanon/libgdx/Views/SearchFlightsView.java
+++ core/src/com/csanon/libgdx/Views/SearchFlightsView.java
@@ -1,12 +1,16 @@
 package com.csanon.libgdx.Views;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.csanon.libgdx.Components.DropDown;
+import com.csanon.libgdx.Components.TextLabel;
 import com.csanon.libgdx.Components.TintedImage;
 
-public class SearchFlightsView extends BaseView{
-	
+public class SearchFlightsView extends BaseView {
+
 	private Button searchButton;
-	private 
+
+	private TextLabel airportLabel, dateLabel;
+	private DropDown airportDropdown;
 
 	public SearchFlightsView() {
 		
@@ -16,25 +20,25 @@ public class SearchFlightsView extends BaseView{
 	@Override
 	public void setSizes() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setPositions() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addActors() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void handle(int outcome) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
