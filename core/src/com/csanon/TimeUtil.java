4a3d850b11fa17b811ab28b0a33822da8e70ec41
magwitch324 <magwitch324@gmail.com>
2016-03-23 15:53:52 -0400

    Fixed try catch block

--- core/src/com/csanon/TimeUtil.java
+++ core/src/com/csanon/TimeUtil.java
@@ -25,7 +25,7 @@ public class TimeUtil {
 	private static DateTimeFormatter dtparseformat = DateTimeFormatter.ofPattern("yyyy MMM dd HH:mm z");
 
 	public static OffsetDateTime string2OffsetDateTime(String aDateTime, double Lat, double Long)
-			throws ParserConfigurationException, IOException, SAXException {
+			throws ParserConfigurationException, IOException, SAXException, UnirestException {
 		int offset = getOffsetByLatLong(Lat, Long);
 		return string2OffsetDateTime(aDateTime, offset);
 	}
@@ -42,47 +42,48 @@ public class TimeUtil {
 	}
 
 	public static int getOffsetByLatLong(double Lat, double Long)
-			throws ParserConfigurationException, IOException, SAXException {
+			throws ParserConfigurationException, IOException, SAXException, UnirestException {
 		String result = null;
 		try {
 			HttpRequest request = Unirest.get("http://api.timezonedb.com").queryString("lat", new Double(Lat))
 					.queryString("lng", new Double(Long)).queryString("key", "NWZDDPVDUNKW");
 			HttpResponse<String> response = request.asString();
 			result = response.getBody();
+
+			/**
+			 * load the xml string into a DOM document check whether the result
+			 * is valid and then return the offset
+			 */
+			try {
+				DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+				InputSource inputSource = new InputSource();
+				inputSource.setCharacterStream(new StringReader(result));
+
+				Document docTimezone = docBuilder.parse(inputSource);
+
+				Element topelement = docTimezone.getDocumentElement();
+				String status = topelement.getAttributeNode("status").getValue();
+
+				if (!status.equals("OK")) {
+					// TODO : throw exception saying not a valid message
+				}
+
+				int offset = Integer.parseInt(topelement.getAttributeNode("gmtOffset").getValue());
+
+				return offset;
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+				throw e;
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw e;
+			} catch (SAXException e) {
+				e.printStackTrace();
+				throw e;
+			}
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		/**
-		 * load the xml string into a DOM document check whether the result is
-		 * valid and then return the offset
-		 */
-		try {
-			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-			InputSource inputSource = new InputSource();
-			inputSource.setCharacterStream(new StringReader(result));
-
-			Document docTimezone = docBuilder.parse(inputSource);
-
-			Element topelement = docTimezone.getDocumentElement();
-			String status = topelement.getAttributeNode("status").getValue();
-
-			if (!status.equals("OK")) {
-				// TODO : throw exception saying not a valid message
-			}
-
-			int offset = Integer.parseInt(topelement.getAttributeNode("gmtOffset").getValue());
-
-			return offset;
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-			throw e;
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw e;
-		} catch (SAXException e) {
 			e.printStackTrace();
 			throw e;
 		}
