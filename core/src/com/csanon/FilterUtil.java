5b09c852dfe28b912578731a3a326ffa23601e94
Sean Amos <sjamos@wpi.edu>
2016-04-26 14:37:48 -0400

    Added more of the test for round trip and added filter by trip

--- core/src/com/csanon/FilterUtil.java
+++ core/src/com/csanon/FilterUtil.java
@@ -9,16 +9,22 @@ public class FilterUtil {
 	public static List<ITrip> filterByEconomy(List<ITrip> trips) {
 		return filterBySeat(trips, SeatClass.ECONOMY);
 	}
-	
+
 	public static List<ITrip> filterByFirstClass(List<ITrip> trips) {
 		return filterBySeat(trips, SeatClass.FIRSTCLASS);
 	}
-	
+
 	public static List<ITrip> filterBySeat(List<ITrip> trips, SeatClass seatType) {
 		Predicate<ITrip> filter = trip -> trip.getSeatType() == seatType;
 		return filterer(trips, filter);
 	}
-	
+
+	public static List<ITrip> filterByTrip(ITrip filtertrip, List<ITrip> returnTrips) {
+		Predicate<ITrip> filter = trip -> filtertrip.getLegs().get(filtertrip.getLegs().size() - 1).getArrivalTime()
+				.getDifference(trip.getLegs().get(0).getDepartureTime()).toMinutes() >= 60;
+		return filterer(returnTrips, filter);
+	}
+
 	private static List<ITrip> filterer(List<ITrip> trips, Predicate<ITrip> filter) {
 		return trips.stream().filter(filter).collect(Collectors.toList());
 	}
